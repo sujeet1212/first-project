5aabd58dd83c3bf2ea77af96e7e56c29b551b5c0
sujeet <sujeetgour1212@gmail.com>
2026-04-17 09:29:46 +0000

    added main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0f5ee92e2d63afc18"   # Ubuntu AMI (Mumbai)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
